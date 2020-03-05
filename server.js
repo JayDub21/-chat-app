69b69d933d30f823e762e3ead2c914a24c2be3b3
JayDub21 <justin.wofford.21@gmail.com>
2020-03-05 11:14:12 -0600

    changed express setup

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
-var app = require("express")();
+var express = require("express");
+var app = express();
 var http = require("http").createServer(app);
 var io = require("socket.io")(http, { origins: "*:*" });
 const PORT = process.env.PORT || 3002;
