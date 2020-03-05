e408f9680271dbfd741d485561d4eb00495cbbc3
JayDub21 <justin.wofford.21@gmail.com>
2020-03-05 10:58:43 -0600

    package change

---
+++ client/build/precache-manifest.f7b45121fc8810559a6d46fdd8acb5ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7c066c5d6e03d3c025baa7eb0b6c39c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ad8a6b3b8f10e575549",
+    "url": "/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "3167b98c693984996e36",
+    "url": "/static/js/2.adb96580.chunk.js"
+  },
+  {
+    "revision": "f0b25a5e5afe7b057a262e459f8d43f1",
+    "url": "/static/js/2.adb96580.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ad8a6b3b8f10e575549",
+    "url": "/static/js/main.6de0f3fe.chunk.js"
+  },
+  {
+    "revision": "477b3bd0c569e2ac5c55",
+    "url": "/static/js/runtime-main.2adec5ec.js"
+  }
+]);
